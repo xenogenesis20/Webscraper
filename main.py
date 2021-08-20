b7a2c578a97d3dd5c8aa57d463999709254c02fb
xenogenesis20 <78182780+xenogenesis20@users.noreply.github.com>
2021-08-19 20:53:59 -0400

    inital

---
+++ main.py
@@ -0,0 +1,30 @@
+from selenium.common.exceptions import TimeoutException
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from decouple import config
+import time
+
+USER = config('USER')
+PW = config('PW')
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+# driver.maximize_window()
+driver.implicitly_wait(3)
+driver.get("https://www.genedx.com/signin/")
+wait = WebDriverWait(driver, 10)
+wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@id='catapultCookie']"))).click()
+wait.until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "div#biopeople-login-registration iframe")))
+wait.until(EC.element_to_be_clickable((By.ID, "loginEmail"))).send_keys(USER)
+wait.until(EC.element_to_be_clickable((By.ID, "passwordEmail"))).send_keys(PW)
+driver.find_element_by_xpath("//button[@type='submit']").click()
+driver.switch_to.default_content()
+
+# driver.find_element_by_xpath("//tbody/tr[3]/td[9]/a[1]/i[1]").click()
+
+# close browser after 5 second delay
+# time.sleep(5)
+# driver.close()
