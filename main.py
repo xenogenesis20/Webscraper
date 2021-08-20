85bdaa32e7b16d51986ffee9802e43b76821afd1
Maikel Martinez <maikel@wemindinstitute.com>
2021-08-20 11:19:09 -0400

    back tracking, unable to select report

--- main.py
+++ main.py
@@ -23,23 +23,30 @@ wait.until(EC.element_to_be_clickable((By.ID, "passwordEmail"))).send_keys(PW)
 driver.find_element_by_xpath("//button[@type='submit']").click()
 driver.switch_to.default_content()
 
-try:
-    n = 0
-    for i in range(2, 23):
-        n = i
-        report = driver.find_element_by_xpath(f"//tbody/tr[{n}]/td[9]/a[1]/i[1].")
-        print(n)
-        if report:
 
-            report.click()
-            time.sleep(3)
-        elif n == 22:
-            next_page = driver.find_element_by_link_text("2")
-            next_page.click()
-except:
-    driver.quit()
+go_next = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Next')]")))
+go_next.click()
+
+report = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "far fa-file-pdf" )))
+report.click()
+
+# try:
+#     n = 0
+#     for i in range(2, 23):
+#         n = i
+#         report = driver.find_element_by_xpath(f"//tbody/tr[{n}]/td[9]/a[1]/i[1].")
+#         print(n)
+#         if report:
+#
+#             report.click()
+#             time.sleep(3)
+#         elif n == 22:
+#             next_page = driver.find_element_by_link_text("2")
+#             next_page.click()
+# except:
+#     driver.quit()
 
 
 # close browser after 5 second delay
-# time.sleep(5)
-# driver.close()
+time.sleep(5)
+driver.close()
